a2e231bdd7060c68084ac1422daf75b03df64a1b
Maksimka <maksimzagradskrv50910@gmail.com>
2025-04-07 22:09:07 +0300

    Writed some main rules

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,51 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <stdexcept>
 
 using namespace std;
+
+class commonLexem {
+private:
+	std::string lex;
+	size_t lineIndex;
+
+public:
+	commonLexem(std::string& s, size_t ind) : lex(s), lineIndex(ind) {
+		// Allowed Lexem: 
+		// 1. key word:
+		// 1.1 data types: int, double, void (functions only), var[size]
+		// 1.2 operators: if(){}, else{}, while(){}, print()
+		// 1.3 return
+		// 2. operators:
+		// 2.1 numeric: +, -, *, /, +=, -=, *=, /=
+		// 2.2 logic: &&, ||, ==, !=, <, <=, >, >=
+		// 2.3 special: =
+		// 3. numbers: without . or e - integer, with . or e - real
+		// 4. special symbols: (, ), ;, {, }, " (for print())
+		//
+		// Rules for program:
+		// 1. After each ; or { or } there is a next line or ' '
+		// 2. If, While, Else blocks must be signed with {}
+		// 3. C syntax
+	}
+};
 
 void printFileContent(std::vector<std::string>& v) {
 	for (int i = 0; i < v.size(); ++i) {
 		cout << v[i] << endl;
 	}
 	cout << endl;
+}
+
+void checkFileContent(std::vector<std::string>& v) {
+	std::string str;
+	for (size_t strInd = 0; strInd < v.size(); ++strInd) {
+		str = v[strInd];
+		for (size_t i = 0; i < str.length(); ++i) {
+			if (str[i] == ';') for (; i < str.length(); ++i) if (str[i] != ' ') throw std::runtime_error("Line " + std::to_string(strInd) + ", symbol " + std::to_string(i) + ": " + str[i]);
+		}
+	}
 }
 
 int main() {
